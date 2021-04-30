50da0192edb3b3366acd7fad32dd29d8e888f07f
hassan214241 <hassan214241@gmail.com>
2021-04-30 22:54:51 +0500

    Updates

---
+++ precache-manifest.de866c682bea53de648c46e10b1c02e9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "438d16728721831aa004f1005141263a",
+    "url": "/crud-Table/index.html"
+  },
+  {
+    "revision": "f32ec5fab8fa468cf1da",
+    "url": "/crud-Table/static/css/main.38f3043c.chunk.css"
+  },
+  {
+    "revision": "5405d9c9988effb8b87b",
+    "url": "/crud-Table/static/js/2.53e02c09.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/crud-Table/static/js/2.53e02c09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfa3d64af450f25109c4",
+    "url": "/crud-Table/static/js/3.bb132f15.chunk.js"
+  },
+  {
+    "revision": "5f983ef0d998e4957796cf6a040e4dc4",
+    "url": "/crud-Table/static/js/3.bb132f15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78b8d61f9ce12d43e115",
+    "url": "/crud-Table/static/js/4.373f47c4.chunk.js"
+  },
+  {
+    "revision": "312b03fc2032f4a26db5e85de50ce98a",
+    "url": "/crud-Table/static/js/4.373f47c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f5cbebdae24a396064a",
+    "url": "/crud-Table/static/js/5.2dc384da.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/crud-Table/static/js/5.2dc384da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f32ec5fab8fa468cf1da",
+    "url": "/crud-Table/static/js/main.17e92bce.chunk.js"
+  },
+  {
+    "revision": "9b3aaf8032ce84fb7480",
+    "url": "/crud-Table/static/js/runtime-main.f9b82fa3.js"
+  }
+]);
